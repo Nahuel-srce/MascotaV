9bb2b867efba4d2a03e0e04e6f0be9453ac62db7
Nahuel <nstorace05@gmail.com>
2025-05-23 10:05:38 -0300

    Subiendo las mentorias del dia

---
+++ info-2025/SEMANA3/MENTORÍAS/Ejercicio04.py
@@ -0,0 +1,37 @@
+# ------------------------------------------------------------------------------
+# Realizá un programa en Python que permita al usuario ingresar 10 números enteros. El programa debe:
+
+# Almacenar los números ingresados en una lista.
+
+# Calcular la suma de todos los números pares.
+
+# Contar cuántos números impares se ingresaron.
+
+# Mostrar por pantalla:
+
+# La lista completa de números ingresados.
+
+# La suma de los números pares.
+
+# La cantidad de números impares
+
+
+numeros = []
+
+cantidad_pares = 0
+cantidad_impares = 0
+
+for i in range(10):
+    num = int(input("Ingrese un número: "))
+    numeros.append(num)
+    
+for n in numeros: 
+    if n % 2 == 0:
+        cantidad_pares += 1
+    else:
+        cantidad_impares += 1
+        
+print("La lista es:", numeros)    
+print("La cantidad de numeros pares que hay es::", cantidad_pares)    
+print("La cantidad de numeros impares que hay es::", cantidad_impares)    
+        
---
+++ info-2025/SEMANA3/MENTORÍAS/Tuplas.py
@@ -0,0 +1,21 @@
+
+
+print("---Tuplas---")
+
+frutas = ("manzana", "sandia", "cereza")
+verduras = ("cebolla", "lechuga", "zapallo")
+
+tupla3 = frutas + verduras
+print(f"Concatenacion de tuplas: {tupla3}")
+
+
+#print(f"Frutas (tupla) {frutas}, tipo: {type(frutas)}")
+# print("Elemento del indice 0", frutas[0])
+# print("Elemento del indice 2", frutas[2])
+
+# #Longitud de una tupla
+
+# print("La longitud de la tupla es:", len(frutas))
+
+# print("la fruta manzana aparece", frutas.count("manzana"), "vez")
+# print("el indice de cereza es:", frutas.index("cereza"))
