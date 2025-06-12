ebbb91555db4e0c0d4563839a4895e6683026096
Nahuel <nstorace05@gmail.com>
2025-06-11 23:46:31 -0300

    Delete cumpleañosA directory

--- cumpleañosA/script.js
+++
@@ -1,8 +0,0 @@
-function mostrarMensaje() {
-  const musica = document.getElementById("musica");
-  const notas = document.getElementById("notas");
-
-  notas.style.display = "block";
-
-  musica.play();
-}
