09e04958a515965a0c6044368cfa6909d1074784
Nahuel <nstorace05@gmail.com>
2025-07-10 09:27:48 -0300

    Delete info-2025 directory

--- info-2025/SEMANA3/CLASE4/ejercicio4.py
+++
@@ -1,7 +0,0 @@
-numero = int(input("Ingrese un número: \n"))
-
-print(f"--- Tabla de Multiplicar del número {numero} --- \n")
-      
-for i in range (1, 11):
-    calculo = numero * i
-    print(f"{numero} x {i} = {calculo}")
--- info-2025/SEMANA5/CLASE8/modulos.py
+++
@@ -1,9 +0,0 @@
-from math import pi
-import datetime
-
-print(pi)
-
-def calculo_area_circulo(radio):
-    pi * radio ** 2
-    
-print(calculo_area_circulo(2))
